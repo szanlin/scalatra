cf89d997358f5bc483f88cf4c5b0d4d2dd844a7a
Ross A. Baker <baker@alumni.indiana.edu>
2012-02-16 01:38:57 -0500

    Services can be chained with orElse.

--- core/src/main/scala/org/scalatra/Service.scala
+++ core/src/main/scala/org/scalatra/Service.scala
@@ -2,4 +2,17 @@ package org.scalatra
 
 import javax.servlet.http.HttpServletRequest
 
-trait Service extends (HttpServletRequest => Result)
+trait Service extends (HttpServletRequest => Result) {
+  def orElse(service: Service) = Service { request =>
+    this(request) match {
+      case NoResult => service(request)
+      case result => result
+    }
+  }
+}
+
+object Service {
+  implicit def apply(f: HttpServletRequest => Result) = new Service {
+    def apply(req: HttpServletRequest): Result = f(req)
+  }
+}
